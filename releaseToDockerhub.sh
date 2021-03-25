110b59031c50a274679660a60c55003aaed34af1
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-25 09:17:51 +0100

    dockerhub release scripts

---
+++ releaseToDockerhub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z ${1+x} ]
+  then
+  echo "Please define semver to release. eg: release.sh 1.0.1"
+  exit 1
+  else
+  echo "building versions '$1'"
+  ../ringface-gui/dockerImagesBuild.sh $1
+  ../ringface-classifier/dockerImageBuild.sh $1
+  ../ringface-connector/dockerImagesBuild.sh $1
+
+  echo "pushin version '$1'"
+  ../ringface-gui/dockerImagesPush.sh  $1
+  ../ringface-classifier/dockerImagePush.sh $1
+  ../ringface-connector/dockerImagesPush.sh $1
+
+  echo "replacing the .env to '$1'"
+  sed -i '' '/VERSION/d' ./.env
+  echo "VERSION=$1" >> ./.env
+fi
